447ab00f338ef1fbdb8de112b0165f178de33d77
Mary <marieshix98@gmail.com>
2024-09-19 15:36:21 +0300

    add clear tasks functionality

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,7 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { ColumnType, Tasktype } from "../types";
-
+import { useKanbanBoard } from "../context/appContext";
+import { MdDelete } from "react-icons/md";
 interface Props {
     item: ColumnType, addTask: (id: string
         | number
@@ -11,15 +12,9 @@ interface Props {
 
 }
 const Card = ({ item, addTask, taskInput, setTaskInput, tasks }: Props) => {
-    const [activeColumnId, setActiveColumnId] = useState<string | number>("");
     const [isOpen, setIsOpen] = useState(false);
     const [taskForm, setTaskForm] = useState(false)
-    const [task, setTask] = useState('')
-    // useEffect(() => {
-    //     if (activeColumnId === item.id) {
-    //         addTask(activeColumnId);
-    //     }
-    // }, [activeColumnId, item.id]);
+    const { handleTaskDelete, handleColumnDelete, clearColumn } = useKanbanBoard()
     const handleOpen = () => {
         setIsOpen((prev) => !prev);
     };
@@ -29,14 +24,15 @@ const Card = ({ item, addTask, taskInput, setTaskInput, tasks }: Props) => {
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTaskInput(event.target.value);
     };
+    const handleColumnEdit = () => {
+
+    }
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         addTask(item.id)
         setTaskForm(false)
         setTaskInput('')
-
     }
-
     return (
         <div>
             <p>{item.id}</p>
@@ -47,14 +43,15 @@ const Card = ({ item, addTask, taskInput, setTaskInput, tasks }: Props) => {
             <div>
                 {isOpen && (
                     <ul>
-                        <li>Clear</li>
+                        <li onClick={() => clearColumn(item.id)}>Clear</li>
                         <li>Update</li>
-                        <li>Delete</li>
+                        <li onClick={() => handleColumnDelete(item.id)}>Delete</li>
                     </ul>
                 )}
             </div>
             {tasks?.length > 0 && tasks?.map(task => <ul key={task.id}>
                 <li>{task.task}</li>
+                <MdDelete onClick={() => handleTaskDelete(task.id)} />
             </ul>)}
             {taskForm && <form onSubmit={handleSubmit}>
                 <input type="text" onChange={handleChange} value={taskInput} />
@@ -63,7 +60,6 @@ const Card = ({ item, addTask, taskInput, setTaskInput, tasks }: Props) => {
 
             <button
                 onClick={() => {
-                    setActiveColumnId(item.id);
                     handleTaskForm()
                 }}
             >
