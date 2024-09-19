505c440b9534608d10f9ec29236e9ec6f57d8bfb
Mary <marieshix98@gmail.com>
2024-09-19 15:33:38 +0300

    setup the application context

---
+++ client/src/context/appContext.tsx
@@ -0,0 +1,104 @@
+import { ReactNode, useState, useEffect, useContext, createContext, useCallback } from "react";
+import { ColumnType, Tasktype } from "../types";
+import { generateUniqueId } from "../utils";
+interface Props {
+    children: ReactNode
+}
+
+interface initialStateType {
+    allTasks: Tasktype[],
+    setAllTasks: React.Dispatch<React.SetStateAction<Tasktype[]>>,
+    taskInput: string,
+    setTaskInput: React.Dispatch<React.SetStateAction<string>>,
+    columns: ColumnType[],
+    setColumns: React.Dispatch<React.SetStateAction<ColumnType[]>>,
+    columnName: string,
+    setColumnName: React.Dispatch<React.SetStateAction<string>>,
+    isColumnFormOpen: boolean,
+    setIsColumnFormOpen: React.Dispatch<React.SetStateAction<boolean>>,
+    handleColumnDelete: (x: string | number) => void,
+    handleTaskDelete: (x: string) => void,
+    clearColumn: (x: string | number) => void,
+    addColumn: (event: React.FormEvent<HTMLFormElement>) => void,
+    createTask: (columnId: string | number) => void,
+    editColumn: (column: ColumnType) => void,
+}
+const TaskManagerContext = createContext<initialStateType>({
+    allTasks: [],
+    setAllTasks: () => { },
+    taskInput: '',
+    setTaskInput: () => { },
+    columnName: '',
+    setColumnName: () => { },
+    columns: [],
+    setColumns: () => { },
+    isColumnFormOpen: false,
+    setIsColumnFormOpen: () => { },
+    handleColumnDelete: () => { },
+    handleTaskDelete: () => { },
+    clearColumn: () => { },
+    addColumn: () => { },
+    createTask: () => { },
+    editColumn: () => { }
+});
+export const useKanbanBoard = () => {
+    return useContext(TaskManagerContext)
+}
+
+const KanbanBoardProvider = ({ children }: Props) => {
+    const initialTasksState = localStorage.getItem('tasks')
+        ? JSON.parse(localStorage.getItem('tasks') || '')
+        : []
+    const initialColumnState = localStorage.getItem('columns') ? JSON.parse(localStorage.getItem('columns') || '')
+        : []
+    const [allTasks, setAllTasks] = useState<Tasktype[]>(initialTasksState)
+    const [columns, setColumns] = useState<ColumnType[]>(initialColumnState)
+    const [taskInput, setTaskInput] = useState('')
+    const [columnName, setColumnName] = useState('')
+    const [isColumnFormOpen, setIsColumnFormOpen] = useState(false)
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(allTasks))
+        localStorage.setItem('columns', JSON.stringify(columns))
+    }, [allTasks, columns])
+
+    const addColumn = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const column = {
+            id: generateUniqueId(),
+            title: columnName,
+        };
+        setColumns([...columns, column]);
+        setColumnName('')
+        setIsColumnFormOpen(false)
+    }
+    const createTask = (columnId: string | number) => {
+        const newTask = {
+            id: generateUniqueId(),
+            task: taskInput,
+            columnId
+        }
+        setAllTasks([...allTasks, newTask])
+    }
+    const handleColumnDelete = useCallback((columnId: string | number) => {
+        const filteredColumns = columns.filter(column => column.id !== columnId)
+        setColumns(filteredColumns)
+    }, [columns])
+
+    const handleTaskDelete = useCallback((taskId: string) => {
+        const filteredTasks = allTasks.filter(task => task.id !== taskId)
+        setAllTasks(filteredTasks)
+    }, [allTasks])
+
+    const clearColumn = (columnId: string | number) => {
+        const clearTasks = allTasks.filter(task => task.columnId !== columnId)
+        setAllTasks(clearTasks)
+    }
+    const editColumn = (formValue: ColumnType) => {
+        const { id, title } = formValue
+        const updatedColumns = columns.map(column => column.id === id ? { id, title } : column)
+        setColumns(updatedColumns)
+    }
+    return <TaskManagerContext.Provider value={{ allTasks, setAllTasks, taskInput, setTaskInput, columnName, setColumnName, columns, setColumns, handleColumnDelete, handleTaskDelete, clearColumn, isColumnFormOpen, setIsColumnFormOpen, addColumn, createTask, editColumn }}>{children}</TaskManagerContext.Provider>
+}
+export default KanbanBoardProvider
