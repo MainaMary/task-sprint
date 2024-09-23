1bf1aa69722d99a922c4c85cd91836b2da46105e
Mary <marieshix98@gmail.com>
2024-09-23 23:58:10 +0300

    create text input

---
+++ client/src/components/TextFieldInput.tsx
@@ -0,0 +1,22 @@
+import { TextField } from "@mui/material";
+interface TextFieldProps {
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+    label: string;
+    value: string
+
+}
+const TextFieldInput = ({ onChange, label, value }: TextFieldProps) => {
+    return (
+        <TextField
+            id="outlined-controlled"
+            label={label}
+            value={value}
+            onChange={onChange}
+            focused
+            required
+            style={{ padding: "4px" }}
+        />
+    )
+}
+
+export default TextFieldInput
