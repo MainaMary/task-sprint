3dd2f98a726f9a4bb1169fb4919438559ebb8807
MainaMary <marieshix98@gmail.com>
2024-10-27 23:30:11 +0300

    wrap the entire app with Apollo Provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,12 +3,20 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import KanbanBoardProvider from './context/appContext.tsx'
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client'
 
+const client = new ApolloClient({
+  uri:"http://localhost:4000",
+  cache: new InMemoryCache()
+})
 // //Wrap your application with the KanbanProvider to provide access to the context.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <ApolloProvider client={client}>
     <KanbanBoardProvider>
       <App />
     </KanbanBoardProvider>
+    </ApolloProvider>
+    
   </StrictMode>,
 )
